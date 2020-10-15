fa974b538deec79ceb9ceef10865e370a72e99a0
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-10-15 16:54:15 +0200

    Added static routing

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/AboutPage">About</NavLink>
+      <NavLink to="/DiscoverMoviesPage">Discover Movies</NavLink>
+    </div>
+  );
+}
