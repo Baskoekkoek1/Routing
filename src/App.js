dc59bf5b31110d69b1eef86cf7f40bd8a7333731
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-10-16 11:14:46 +0200

    fixed the discover movie page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar/NavBar";
+import NavBar from "./Components/NavBar/NavBar/NavBar";
 import DiscoverMoviesPage from "./Pages/DiscoverMoviesPage";
 import AboutPage from "./Pages/AboutPage";
 import Homepage from "./Pages/Homepage";
-
-function Discover() {
-  return <h1>Discover</h1>;
-}
-
-function About() {
-  return <h1>About</h1>;
-}
-
-function Home() {
-  return <h1>Home</h1>;
-}
 
 function App() {
   return (
--- src/Pages/DiscoverMoviesPage.js
+++ src/Pages/DiscoverMoviesPage.js
@@ -1,22 +1,71 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function DiscoverMoviesPage() {
   const [searchText, set_searchText] = useState("");
+  const [movies, setMovie] = useState({ status: "idle", data: null });
 
-  const search = () => {
-    console.log("TODO search movies for:", searchText);
+  const search = async () => {
+    console.log("Start searching for:", searchText);
+
+    // Best practice: encode the string so that special characters
+    //  like '&' and '?' don't accidentally mess up the URL
+    const queryParam = encodeURIComponent(searchText);
+    setMovie({ status: "searching...", data: null });
+    // Option B: use the `axios` library like we did on Tuesday
+    const response = await axios.get(
+      `https://omdbapi.com/?apikey=2344c2a6&s=${queryParam}`
+    );
+    setMovie({ status: "done", data: response.data.Search });
+
+    console.log("Success!", response.data.Search);
   };
 
-  return (
-    <div>
-      <h1>Discover some movies!</h1>
-      <p>
-        <input
-          value={searchText}
-          onChange={(e) => set_searchText(e.target.value)}
-        />
-        <button onClick={search}>Search</button>
-      </p>
-    </div>
-  );
+  if (movies.data === null) {
+    return (
+      <div>
+        <h1>Discover some movies!</h1>
+        <p>{movies.status}</p>
+        <p>
+          <input
+            value={searchText}
+            onChange={(e) => set_searchText(e.target.value)}
+          />
+          <button onClick={search}>Search</button>
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Discover some movies!</h1>
+        <p>{movies.status}</p>
+        <p>
+          <input
+            value={searchText}
+            onChange={(e) => set_searchText(e.target.value)}
+          />
+          <button onClick={search}>Search</button>
+        </p>
+        <div>
+          {" "}
+          <div>
+            {movies.data.map((movie) => {
+              // movie.key = movie.imdbID;
+              console.log(movie);
+              return (
+                <div>
+                  <Link to={`/movies/${movie.imdbID}`}>
+                    <h3>{movie.Title}</h3>
+                  </Link>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
